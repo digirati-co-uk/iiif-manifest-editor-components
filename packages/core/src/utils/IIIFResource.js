194bb44c60708c89e5274f627f7327b5a466b659
Adam Meszaros <adam.meszaros@digirati.com>
2018-11-29 08:59:20 +0000

    Basic IIIF resource generator

--- packages/core/src/utils/IIIFResource.js
+++ packages/core/src/utils/IIIFResource.js
@@ -1,3 +1,50 @@
+import generateURI from './URIGenerator';
+
+const defaultResourceRenderers = {
+  Manifest: props => ({
+    '@context': [
+      'http://www.w3.org/ns/anno.jsonld',
+      'http://iiif.io/api/presentation/3/context.json',
+    ],
+    type: 'Manifest',
+    label: {
+      label: {
+        en: ['Untitled Manifest'],
+      },
+    },
+    items: [],
+    ...props,
+  }),
+  Canvas: props => ({
+    label: {
+      en: ['Untitled Canvas'],
+    },
+    height: 1000,
+    width: 1000,
+    type: 'Canvas',
+    items: [
+      {
+        type: 'AnnotationPage',
+        items: [],
+      },
+    ],
+    ...props,
+  }),
+  Annotation: props => ({
+    type: 'Annotation',
+    motivation: 'painting',
+    label: {
+      en: ['Untitled Annotation'],
+    },
+    body: {
+      type: 'TextualBody',
+      value: 'Untitled Annotation Body',
+      format: 'text/plain',
+    },
+    ...props,
+  }),
+};
+
 /**
  * Renders the resource template using the passed properties.
  *
@@ -7,8 +54,18 @@
  * @param {String} type - This is the IIIF resource type
  * @param {Object} options - parameters for the renderer
  */
-const renderResource = (type, options) => {
-  return {};
+const renderResource = (type, options = { props: {} }) => {
+  let resource;
+  //TODO: make it overridable
+  if (defaultResourceRenderers.hasOwnProperty(type)) {
+    resource = defaultResourceRenderers[type](options.props);
+  } else {
+    resource = {
+      type,
+    };
+  }
+  generateURI(resource, options.parent);
+  return resource;
 };
 
 export default renderResource;
--- packages/core/src/utils/URIGenerator.js
+++ packages/core/src/utils/URIGenerator.js
@@ -1,10 +1,41 @@
+//TODO: make it configurable
+const rootURL = 'http://digirati.com/iiif/v3/';
+
+const s4 = () =>
+  Math.floor((1 + Math.random()) * 0x10000)
+    .toString(16)
+    .substring(1);
+
+const guid = (pattern = 'ss-s-s-s-sss') => pattern.replace(/s/g, s4);
+
 /**
  * Generates a URI for the resource passed.
  * @param {Object} resource - the resource requires an id or an id refresh
  * @param {Object} parent - the parent resource if applicable
  * @return {Object} the resource with the id
  */
-const generateURI = (resource, parent = null) => {
+const generateURI = (resource, parent = null, options) => {
+  switch (resource.type) {
+    case 'Manifest':
+      //{scheme}://{host}/{prefix}/{identifier}/manifest
+      resource.id = `rootURL${options.id || guid()}/manifest`;
+      break;
+    case 'Canvas':
+    // {scheme}://{host}/{prefix}/{identifier}/canvas/{name}
+    case 'Annotation':
+    // {scheme}://{host}/{prefix}/{identifier}/annotation/{name}
+    case 'AnnotationPage':
+    //	{scheme}://{host}/{prefix}/{identifier}/list/{name}
+    case 'Range':
+    // {scheme}://{host}/{prefix}/{identifier}/range/{name}
+    default:
+      if (parent && parent.id) {
+        resource.id = parent.id.replace(
+          /\/(manifest|canvas|annotation|list|range)(\/.*)?$/,
+          `/${resource.type}/${options.id || guid()}`
+        );
+      } // TODO: else { is this even possible }
+  }
   return resource;
 };
 
