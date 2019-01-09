e9b0077cc39e02b6b2ada17822a452da524c292a
Adam Meszaros <adam.meszaros@digirati.com>
2019-01-09 15:22:08 +0000

    added thumbnails as a reminder, although it needs to use the same interface on the end...

--- packages/core/src/utils/VideoServices.js
+++ packages/core/src/utils/VideoServices.js
@@ -1,3 +1,4 @@
+//TODO: make the thumbnail unified
 export const parseVideo = url => {
   // - Supported YouTube URL formats:
   //   - http://www.youtube.com/watch?v=My2FRPA3Gf8
@@ -18,44 +19,25 @@ export const parseVideo = url => {
       type: 'youtube',
       id: RegExp.$6,
       src: '//www.youtube.com/embed/' + RegExp.$6,
+      thumbnail: '//img.youtube.com/vi/' + RegExp.$6 + '/maxresdefault.jpg',
     };
   } else if (RegExp.$3.indexOf('vimeo') > -1) {
     return {
       type: 'vimeo',
       id: RegExp.$6,
       src: '//player.vimeo.com/video/' + RegExp.$6,
+      thumbnail: cb => {
+        fetch('http://vimeo.com/api/v2/video/' + RegExp.$6 + '.json')
+          .then(response => response.json())
+          .then(data => cb(data[0].thumbnail_large));
+      },
     };
   } else if (RegExp.$3.indexOf('dailymotion.com') > -1) {
     return {
       type: 'dailymotion',
       id: RegExp.$6,
       src: '//www.dailymotion.com/embed/video/' + RegExp.$6,
+      thumbnail: '//www.dailymotion.com/thumbnail/video/' + RegExp.$6,
     };
-  }
-};
-
-export const createVideo = (url, width, height) => {
-  // Returns an iframe of the video with the specified URL.
-  var videoObj = parseVideo(url);
-  var $iframe = $('<iframe>', { width: width, height: height });
-  $iframe.attr('frameborder', 0);
-  if (videoObj.type === 'youtube') {
-    $iframe.attr('src', '//www.youtube.com/embed/' + videoObj.id);
-  } else if (videoObj.type === 'vimeo') {
-    $iframe.attr('src', '//player.vimeo.com/video/' + videoObj.id);
-  }
-  return $iframe;
-};
-
-export const getVideoThumbnail = (url, cb) => {
-  // Obtains the video's thumbnail and passed it back to a callback function.
-  var videoObj = parseVideo(url);
-  if (videoObj.type === 'youtube') {
-    cb('//img.youtube.com/vi/' + videoObj.id + '/maxresdefault.jpg');
-  } else if (videoObj.type === 'vimeo') {
-    // Requires jQuery
-    $.get('http://vimeo.com/api/v2/video/' + videoObj.id + '.json', data =>
-      cb(data[0].thumbnail_large)
-    );
   }
 };
