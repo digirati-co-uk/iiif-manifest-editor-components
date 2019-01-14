9b6bfb0fe42808af4a5443d1c11d941e18ac0b59
Adam Meszaros <adam.meszaros@digirati.com>
2019-01-14 14:05:23 +0000

    refactored upgrader for better readability

--- packages/core/src/utils/IIIFUpgrader.js
+++ packages/core/src/utils/IIIFUpgrader.js
@@ -3,22 +3,23 @@ import Upgrader from 'iiif-prezi2to3';
 const IIIF_PRESENTATION_V3_CONTEXT_NAMESPACE =
   'http://iiif.io/api/presentation/3/context.json';
 
+const DEFAULT_LANG = 'en';
+
+const isP3Context = context =>
+  context &&
+  ((context.constructor === Array &&
+    context.includes(IIIF_PRESENTATION_V3_CONTEXT_NAMESPACE)) ||
+    (context.constructor === String &&
+      context !== IIIF_PRESENTATION_V3_CONTEXT_NAMESPACE));
+
 const convertToV3ifNecessary = manifest => {
   const context = manifest['@context'];
-  const isNotP3 =
-    context &&
-    ((context.constructor === Array &&
-      !context.includes(IIIF_PRESENTATION_V3_CONTEXT_NAMESPACE)) ||
-      (context.constructor === String &&
-        !context !== IIIF_PRESENTATION_V3_CONTEXT_NAMESPACE));
-  if (isNotP3) {
-    return new Upgrader({
-      deref_links: false,
-      default_lang: 'en',
-    }).processResource(manifest, true);
-  } else {
-    return manifest;
-  }
+  return isP3Context(context)
+    ? manifest
+    : new Upgrader({
+        deref_links: false,
+        default_lang: DEFAULT_LANG,
+      }).processResource(manifest, true);
 };
 
 export default convertToV3ifNecessary;
