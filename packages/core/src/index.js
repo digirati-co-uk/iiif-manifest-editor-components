a12b902851878864096cb7b4b9ad1efd96a21d44
Adam Meszaros <adam.meszaros@digirati.com>
2019-01-18 08:59:47 +0000

    support for tudelft now

--- packages/core/src/index.js
+++ packages/core/src/index.js
@@ -1,13 +1,18 @@
-import AnnotationList from './components/AnnotationList/AnnotationList';
-import CanvasList from './components/CanvasList/CanvasList';
-import CollectionExplorer from './components/CollectionExplorer/CollectionExplorer';
-import EditableCanvas from './components/EditableCanvas/EditableCanvas';
-import MetadataEditor from './components/MetadataEditor/MetadataEditor';
+// import AnnotationList from './components/AnnotationList/AnnotationList';
+// import CanvasList from './components/CanvasList/CanvasList';
+// import CollectionExplorer from './components/CollectionExplorer/CollectionExplorer';
+// import EditableCanvas from './components/EditableCanvas/EditableCanvas';
+// import MetadataEditor from './components/MetadataEditor/MetadataEditor';
+import React from 'react';
+import { render } from 'react-dom';
+import TUDelftManifestEditor from './containers/TUDelftManifestEditor';
 
-export default {
-  AnnotationList,
-  CanvasList,
-  CollectionExplorer,
-  EditableCanvas,
-  MetadataEditor,
-};
+render(<TUDelftManifestEditor />, document.getElementById('app'));
+// export default {
+//   AnnotationList,
+//   CanvasList,
+//   CollectionExplorer,
+//   EditableCanvas,
+//   MetadataEditor,
+//   TUDelftManifestEditor,
+// };
