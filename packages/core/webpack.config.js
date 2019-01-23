bf1a1a4965fb7e5df56d00a1bc3d12f20e11ba11
Adam Meszaros <adam.meszaros@digirati.com>
2019-01-23 17:01:14 +0000

    added dotenv-wepback in order to be able read up enviroment variables during build.

--- packages/core/webpack.config.js
+++ packages/core/webpack.config.js
@@ -1,14 +1,10 @@
 const Config = require('webpack-config').default;
-var webpack = require('webpack');
+const Dotenv = require('dotenv-webpack');
 
 module.exports = new Config().extend('@fesk/scripts/webpack').merge({
   plugins: [
-    new webpack.DefinePlugin({
-      'process.env': {
-        AMZN_S3_IDENTITY_POOL_HASH: JSON.stringify(process.env.AMZN_S3_IDENTITY_POOL_HASH),
-        AMZN_S3_REGION: JSON.stringify(process.env.AMZN_S3_REGION),
-        AMZN_S3_BUCKET: JSON.stringify(process.env.AMZN_S3_BUCKET),
-      },
-    }),
-  ],
+    new Dotenv({
+        systemvars: true
+    })
+  ]
 });
