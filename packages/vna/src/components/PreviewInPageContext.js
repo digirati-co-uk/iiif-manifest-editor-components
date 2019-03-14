cb908903a98fa0565df14e94546dd6352df59820
Adam Meszaros <adam.meszaros@digirati.com>
2019-03-14 12:01:05 +0000

    in context preview

---
+++ packages/vna/src/components/PreviewInPageContext.js
@@ -0,0 +1,216 @@
+import React from 'react';
+import vamicons from './vamicons.svg';
+
+const VAMIcon = props => (
+  <div
+    className={props.className}
+    dangerouslySetInnerHTML={{
+      __html:
+        '<svg class="' +
+        props.svgClassName +
+        '"><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="' +
+        vamicons +
+        '#' +
+        props.name +
+        '"></use></svg>',
+    }}
+  />
+);
+
+export default ({ children }) => {
+  return (
+    <div className="page-outer js-page-outer theme-egg-yolk">
+      <link
+        href="https://www.vam.ac.uk/assets/application-eb724ecd1a3aefc09883f691749b17ce4e26a327579d46efe39b1931aee5d398.css"
+        rel="stylesheet"
+      />
+      <div className="nav"> 
+        <div className="banner js-banner" />
+      </div>
+      <main className="main">
+        <header className="main__banner">
+          <div className="main__title">
+            <h1 className="main__heading">
+              Inside an ocean liner: the 'Aquitania'{' '}
+            </h1>
+
+            <div className="main__articlereference">
+              <p className="main__articlereference__ref">
+                Produced as part of{' '}
+                <span className="main__articlereference__ref__exhibname">
+                  Ocean Liners: Speed and Style
+                </span>
+              </p>
+              <p className="main__articlereference__exhibdates">
+                On now until Sunday, 17 June 2018
+              </p>
+              <a
+                className="btn btn--themed btn--light"
+                href="https://www.vam.ac.uk/exhibitions/ocean-liners-speed-style"
+              >
+                Find out more
+              </a>
+            </div>
+          </div>
+          <div className="logo--header">
+            <div className="logo">
+              <VAMIcon
+                className="logo__container"
+                svgClassName="logo__icon themed--color"
+                name="valogo-clipped"
+              />
+            </div>
+          </div>
+        </header>
+        <section className="main__content">
+          <article className="article">
+            <ul className="article__social">
+              <li className="article__social-title">Share</li>
+              <li className="article__social-link">
+                <a
+                  aria-label="Share on Facebook"
+                  href="https://www.facebook.com/sharer/sharer.php?u=https://www.vam.ac.uk/articles/inside-an-ocean-liner-aquitania"
+                >
+                  <div className="social-icon social-icon--onwhite">
+                    <VAMIcon
+                      className="social-icon__container"
+                      svgClassName="social-icon__icon themed--color"
+                      name="facebook"
+                    />
+                  </div>
+                </a>{' '}
+              </li>
+              <li className="article__social-link">
+                <a
+                  aria-label="Share on Twitter"
+                  href="https://twitter.com/home?status=https://www.vam.ac.uk/articles/inside-an-ocean-liner-aquitania"
+                >
+                  <div className="social-icon social-icon--onwhite">
+                    <VAMIcon
+                      className="social-icon__container"
+                      svgClassName="social-icon__icon themed--color"
+                      name="twitter"
+                    />
+                  </div>
+                </a>{' '}
+              </li>
+              <li className="article__social-link">
+                <a
+                  aria-label="Share on Pin trest"
+                  href="https://pinterest.com/pin/create/button/?url=https://www.vam.ac.uk/articles/inside-an-ocean-liner-aquitania&amp;media=V&amp;amp;A&amp;description="
+                >
+                  <div className="social-icon social-icon--onwhite">
+                    <VAMIcon
+                      className="social-icon__container"
+                      svgClassName="social-icon__icon themed--color"
+                      name="pinterest"
+                    />
+                  </div>
+                </a>{' '}
+              </li>
+              <li className="article__social-link">
+                <a href="http://www.tumblr.com/share/link?url=https://www.vam.ac.uk/articles/inside-an-ocean-liner-aquitania">
+                  <div className="social-icon social-icon--onwhite">
+                    <VAMIcon
+                      className="social-icon__container"
+                      svgClassName="social-icon__icon themed--color"
+                      name="tumblr"
+                    />
+                  </div>
+                </a>{' '}
+              </li>
+              <li className="article__social-link">
+                <a
+                  aria-label="Share on Google Plus"
+                  href="https://plus.google.com/share?url=https://www.vam.ac.uk/articles/inside-an-ocean-liner-aquitania"
+                >
+                  <div className="social-icon social-icon--onwhite">
+                    <VAMIcon
+                      className="social-icon__container"
+                      svgClassName="social-icon__icon themed--color"
+                      name="googleplus"
+                    />
+                  </div>
+                </a>{' '}
+              </li>
+            </ul>
+            <p className="article__leadparagraph">
+              Known as 'The Ship Beautiful', Cunard Line's 'Aquitania' (1914 –
+              1950) was considered one of the most elegant ocean liners of the
+              time when it set sail in 1914. A promotional poster from our
+              collection features a beautifully illustrated cross-section view
+              of the ship's interior.
+            </p>
+            <div className="sir-trevor-text">
+              <p>
+                Ocean liners were strictly organised spaces which reflected
+                social hierarchies. The
+                <i> Aquitania</i> provided accommodation for 3,230 passengers,
+                with 618 in first-class, 614 in second-class, and 1,998 in
+                third-class, as well as a crew of 972. First-class passengers
+                occupied the upper, most-spacious areas, while engineers
+                laboured in the boiler room deep down in the hull.
+              </p>
+            </div>
+            <div className="sir-trevor-text">
+              <p>
+                Take a fascinating tour of the <i>Aquitania's</i> interior –
+                from promenade deck to boiler room – with our interactive. Click
+                on the pins to reveal original photographs and stories about the
+                ship's design.{' '}
+              </p>
+            </div>
+
+            <figcaption className="sir-trevor-image__caption">
+              Cunard Line – to all parts of the world, poster, Ulrich Gutersohn,
+              about 1920, England. Museum no. E.1829-2004. © Victoria and Albert
+              Museum, London
+            </figcaption>
+            { children }
+
+            <figcaption className="sir-trevor-image__caption">
+              Cunard Line – to all parts of the world, poster, Ulrich Gutersohn,
+              about 1920, England. Museum no. E.1829-2004. © Victoria and Albert
+              Museum, London
+            </figcaption>
+
+            <section className="b-promo">
+              <article
+                className="b-promo__item js-object-fit-container"
+                style={{
+                  backgroundImage:
+                    "url('https://vanda-production-assets.s3.amazonaws.com/2018/02/05/13/51/52/210f11b2-dfa0-4b18-9825-04d23a4a29fb/960.jpg')",
+                }}
+              >
+                <a
+                  className="b-promo__anchor"
+                  data-tracking="promobox-exhibition"
+                  href="https://www.vam.ac.uk//exhibitions/ocean-liners-speed-style"
+                >
+                  <div className="b-promo__content">
+                    <h1 className="b-promo__type">Exhibition</h1>
+
+                    <p className="b-promo__title">
+                      Ocean Liners: Speed and Style
+                    </p>
+
+                    <p className="b-promo__sponsor" />
+
+                    <div className="u-btn u-btn--arrowed s-themed s-themed--background-color s-themed--background-color--hover s-themed--border-color s-themed--border-color--hover">
+                      Find out more
+                    </div>
+                  </div>
+                </a>
+              </article>
+            </section>
+            <div>
+              <br />
+              <center>© Victoria and Albert Museum, London 2018</center>
+              <br />
+            </div>
+          </article>
+        </section>
+      </main>
+    </div>
+  );
+};
--- packages/vna/src/components/PreviewModal.js
+++ packages/vna/src/components/PreviewModal.js
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import { useState } from 'react';
 import {
   withStyles,
   Dialog,
@@ -10,63 +11,98 @@ import {
   Typography
 } from '@material-ui/core';
 import {
-  Close
+  Close,
+  Fullscreen,
+  Web,
 } from '@material-ui/icons';
 import { FullPageViewer } from '@canvas-panel/full-page-plugin';
+import { PatchworkPlugin } from '@canvas-panel/patchwork-plugin';
 import { SlideShow } from '@canvas-panel/slideshow';
-import  { saveFixtures } from '../utils';
+import { saveFixtures } from '../utils';
+import PreviewInPageContext from './PreviewInPageContext';
+import './Preview/Patchwork.css';
 
-const PreviewModal = ({ manifest, handleClose, open }) => (
-  <Dialog
-    open={open}
-    onClose={handleClose}
-    scroll="paper"
-    fullScreen
-    aria-labelledby="load-manifest-dialog-title"
-    style={{display: 'flex', flexDirection: 'column', }}
-  >
-    <AppBar position="static">
-      <Toolbar>
-        <IconButton color="inherit" onClick={handleClose} aria-label="Close">
-          <Close />
-        </IconButton>
-        <Typography variant="h6" color="inherit" >
-          Preview Experience
-        </Typography>
-      </Toolbar>
-    </AppBar>
-    <div style={{flex: 1, width: '100%'}}>
-      {
-        manifest && 
-        manifest.behavior && 
-        manifest.behavior.filter(behavior=>behavior==='vam-annotated-zoom').length>0 
+const RenderManifest = ({manifest, isDemoPage}) => {
+  const isAnnotatedZoom = 
+    manifest && 
+    manifest.behavior && 
+    manifest.behavior.filter(behavior=>behavior==='vam-annotated-zoom').length>0;
+
+  if (isAnnotatedZoom && isDemoPage) {
+    return (
+      <FullPageViewer
+        jsonLd={saveFixtures(manifest)}
+        title="Preview"
+        annotationPosition="top"
+      >
+        <p>Scroll down to start or click the 'start tour' button.</p>
+        <span className="muted">
+          © Victoria and Albert Museum, London {new Date().getYear()}
+        </span>
+      </FullPageViewer>
+    );
+  } else if (isAnnotatedZoom && !isDemoPage) {
+    return (
+      <div className="patchwork-container">
+        <PatchworkPlugin
+          jsonLdManifest={saveFixtures(manifest)}
+          cssClassMap={{
+            annotation: 'annotation-pin',
+          }}
+          cssClassPrefix="patchwork-"
+          height={500}
+          width={1200}
+        />
+      </div>
+    );
+  } else {
+    return (
+      <SlideShow jsonLd={manifest} />
+    );
+  }
+};
+  
+
+const PreviewModal = ({ manifest, handleClose, open }) => {
+  const [isPage, setIsPage] = useState(false);
+  return (
+    <Dialog
+      open={open}
+      onClose={handleClose}
+      scroll="paper"
+      fullScreen
+      aria-labelledby="load-manifest-dialog-title"
+      style={{display: 'flex', flexDirection: 'column', }}
+    >
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton color="inherit" onClick={handleClose} aria-label="Close">
+            <Close />
+          </IconButton>
+          <Typography variant="h6" color="inherit" >
+            Preview Experience
+          </Typography>
+          <IconButton onClick={()=>setIsPage(!isPage)} color="inherit" aria-label={isPage ? "Web" : "Fullscreen"}>
+            {isPage ? (
+              <React.Fragment><Web /><Typography variant="h6" color="inherit">Show In context</Typography></React.Fragment>
+            ): (
+              <React.Fragment><Fullscreen /><Typography variant="h6" color="inherit">FullPage</Typography></React.Fragment>
+            )}
+          </IconButton>
+        </Toolbar>
+      </AppBar>
+      {isPage 
         ? (
-          <FullPageViewer
-            jsonLd={saveFixtures(manifest)}
-            title="Preview"
-            annotationPosition="top"
-          >
-            <p>Scroll down to start or click the 'start tour' button.</p>
-            <span className="muted">
-              © Victoria and Albert Museum, London 2018
-            </span>
-          </FullPageViewer>
-          // <PatchworkPlugin
-          //   jsonLdManifest={saveFixtures(manifest)}
-          //   cssClassMap={{
-          //     annotation: 'annotation-pin',
-          //   }}
-          //   cssClassPrefix="patchwork-"
-          //   height={500}
-          //   width={1200}
-          // />
-        )
-        : (
-          <SlideShow jsonLd={manifest} />
-        )
-      }
-    </div>
-  </Dialog>
-);
+          <div style={{flex: 1, width: '100%'}}>
+            <RenderManifest manifest={manifest} isDemoPage={isPage}/>
+          </div>
+        ) : (
+          <PreviewInPageContext>
+            <RenderManifest manifest={manifest} />
+          </PreviewInPageContext>
+        )}
+    </Dialog>
+  );
+}
 
 export default PreviewModal;
