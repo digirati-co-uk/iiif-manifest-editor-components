6e8dc360e467a3a70a74713ff20d8114805ab04d
Adam Meszaros <adam.meszaros@digirati.com>
2019-03-27 21:41:54 +0000

    demosite has it's onwn project

--- packages/demosite/src/index.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import { render } from 'react-dom';
-
-//Start simple editor manifest editor
-import SimpleEditorUI from './containers/SimpleEditorUI';
-render(<SimpleEditorUI />, document.getElementById('app'));
