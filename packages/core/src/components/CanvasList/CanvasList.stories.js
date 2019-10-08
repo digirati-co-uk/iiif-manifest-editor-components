d6a375ff9a0ba4e876a4fd628fc3482192a03f09
Kelsie <kelsie.braidwood@digirati.com>
2019-10-08 15:57:15 +0100

    Adds CanvasList story

---
+++ packages/core/src/components/CanvasList/CanvasList.stories.js
@@ -0,0 +1,30 @@
+import * as React from 'react';
+import { storiesOf } from '@storybook/react';
+import { withKnobs, text, boolean } from '@storybook/addon-knobs';
+
+import CanvasList from './CanvasList.js';
+import { DragDropContext } from 'react-beautiful-dnd';
+
+storiesOf('CanvasList', module)
+  .addDecorator(withKnobs)
+  .add('CanvasList', () => {
+    const TEST_CANVASES = [
+      {
+        id: 'test-canvas-1',
+        type: 'Canvas',
+      },
+      {
+        id: 'test-canvas-2',
+        type: 'Canvas',
+      },
+    ];
+    return (
+      <DragDropContext onDragEnd={() => {}}>
+        <CanvasList
+          direction={text('Horizontal', 'horizontal')}
+          canvases={TEST_CANVASES}
+          selected={text('Selected', 'test-canvas-1')}
+        />
+      </DragDropContext>
+    );
+  });
