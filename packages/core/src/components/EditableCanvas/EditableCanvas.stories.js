22c0ab4f5f79e67fd11563a419014f2a5b745739
Kelsie <kelsie.braidwood@digirati.com>
2019-10-08 16:32:42 +0100

    Adds story for EditableCanvasPanel

--- packages/core/src/components/EditableCanvas/EditableCanvas.stories.js
+++ packages/core/src/components/EditableCanvas/EditableCanvas.stories.js
@@ -1,13 +1,11 @@
 import * as React from 'react';
 import { storiesOf } from '@storybook/react';
-import { withKnobs, boolean } from '@storybook/addon-knobs';
+import { withKnobs } from '@storybook/addon-knobs';
 
 import EditableCanvas from './EditableCanvas.js';
 
 storiesOf('EditableCanvas', module)
   .addDecorator(withKnobs)
   .add('EditableCanvas', () => {
-    return (
-      <EditableCanvas />
-    );
+    return <EditableCanvas />;
   });
