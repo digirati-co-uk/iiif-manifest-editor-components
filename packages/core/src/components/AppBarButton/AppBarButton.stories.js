1afef7fe805cca78f86e20e32930c54e63d4c4ee
Kelsie <kelsie.braidwood@digirati.com>
2019-10-08 09:15:26 +0100

    Adds stories for the AppBarButton

---
+++ packages/core/src/components/AppBarButton/AppBarButton.stories.js
@@ -0,0 +1,27 @@
+import * as React from 'react';
+import { storiesOf } from '@storybook/react';
+import { withKnobs, text } from '@storybook/addon-knobs';
+import AppBarButton from './AppBarButton.js';
+
+storiesOf('AppBarButton', module)
+  .addDecorator(withKnobs)
+  .add('AppBarButton with Label', () => {
+    return (
+      <AppBarButton
+        icon={':)'}
+        text={text('Label', 'Update me')}
+        onClick={() => {}}
+        variant={'icon-and-label'}
+      />
+    );
+  })
+  .add('AppBarButton Slim', () => {
+    return (
+      <AppBarButton
+        icon={':)'}
+        text={text('Label', 'Update me')}
+        onClick={() => {}}
+        variant={'icon-and-tooltip'}
+      />
+    );
+  });
