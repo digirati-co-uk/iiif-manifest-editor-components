31f069fc7741139c5e553c1a8929f9646594c1f7
Kelsie <kelsie.braidwood@digirati.com>
2019-10-08 09:53:39 +0100

    Adds story for default tooltip

---
+++ packages/core/src/components/DefaultTooltip/DefaultTooltip.stories.js
@@ -0,0 +1,17 @@
+import * as React from 'react';
+import { storiesOf } from '@storybook/react';
+import { withKnobs, text } from '@storybook/addon-knobs';
+
+import DefaultTooltip from './DefaultTooltip.js';
+
+storiesOf('DefaultTooltip', module)
+  .addDecorator(withKnobs)
+  .add('DefaultTooltip with Label', () => {
+    return (
+      <DefaultTooltip
+        title={text('Text', 'Update me')}
+        children={<div>Hover over me</div>}
+        variant={'icon-and-label'}
+      />
+    );
+  });
