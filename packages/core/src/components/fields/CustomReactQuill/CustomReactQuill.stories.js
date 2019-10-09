02f13492d24dc2700fcd0817a2a91cc84b14c3f1
Kelsie <kelsie.braidwood@digirati.com>
2019-10-09 16:33:58 +0100

    Adds CustomReactQuillPopupForm with opclasses option addon

--- packages/core/src/components/fields/CustomReactQuill/CustomReactQuill.stories.js
+++ packages/core/src/components/fields/CustomReactQuill/CustomReactQuill.stories.js
@@ -1,22 +1,41 @@
 import * as React from 'react';
 import { storiesOf } from '@storybook/react';
-import { withKnobs, text } from '@storybook/addon-knobs';
-import { DragDropContext } from 'react-beautiful-dnd';
+import { withKnobs, array, boolean, optionsKnob } from '@storybook/addon-knobs';
 import CustomReactQuill from './CustomReactQuill.js';
+import CustomReactQuillPopupForm from './CustomReactQuill.PopupForm.js';
 
+const config = {
+  toolbar: {
+    container: [
+      array('Headers', [{ header: [1, 2, false] }]),
+      ['bold', 'italic'],
+      ['link', 'blockquote', 'image', 'video'],
+      [{ list: 'ordered' }, { list: 'bullet' }],
+    ],
+  },
+};
 storiesOf('CustomReactQuill', module)
   .addDecorator(withKnobs)
   .add('CustomReactQuill', () => {
-    const config = {
-      toolbar: {
-        container: [
-          [{ header: [1, 2, false] }],
-          ['bold', 'italic'],
-          ['link', 'blockquote', 'image', 'video'],
-          [{ list: 'ordered' }, { list: 'bullet' }],
-        ],
-      },
-    };
-
     return <CustomReactQuill editorConfig={config} />;
+  })
+  .add('CustomReactQuill.PopupForm', () => {
+    const values = {
+      One: 'one',
+      Two: 'two',
+      Three: 'three',
+    };
+    return (
+      <CustomReactQuillPopupForm
+        children={<CustomReactQuill editorConfig={config} />}
+        open={boolean('Open', true)}
+        classes={optionsKnob(
+          'Classes',
+          values,
+          'one',
+          { display: 'inline-radio' },
+          'Classes'
+        )}
+      />
+    );
   });
