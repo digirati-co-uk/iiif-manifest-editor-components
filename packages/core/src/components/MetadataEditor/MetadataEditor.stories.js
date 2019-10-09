59d04ba719ce2a1409d8e897c75ff2bc3b1d40c3
Kelsie <kelsie.braidwood@digirati.com>
2019-10-09 11:59:21 +0100

    Adds basic set up of the MetadataEditor story

---
+++ packages/core/src/components/MetadataEditor/MetadataEditor.stories.js
@@ -0,0 +1,27 @@
+import * as React from 'react';
+import { storiesOf } from '@storybook/react';
+import { withKnobs, text, boolean } from '@storybook/addon-knobs';
+import MetadataEditor from './MetadataEditor.js';
+
+const TEST_CANVAS = {
+  id: 'test-canvas-1',
+};
+const TEST_ANNOTATION = {
+  id: 'test-annotation-1',
+};
+const TEST_MANIFEST = {
+  id: 'test-manifest-1',
+};
+storiesOf('MetadataEditor', module)
+  .addDecorator(withKnobs)
+  .add('MetadataEditor', () => {
+    return (
+      <MetadataEditor
+        manifest={TEST_MANIFEST}
+        canvas={TEST_CANVAS}
+        annotation={TEST_ANNOTATION}
+        lang={'en'}
+        fieldConfig={[]}
+      />
+    );
+  });
