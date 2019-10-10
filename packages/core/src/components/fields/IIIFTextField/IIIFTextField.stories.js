46a2701fd26f0dc36388ee072a0cc4d533d81ffc
Kelsie <kelsie.braidwood@digirati.com>
2019-10-10 13:29:56 +0100

    Adds further changes to the IIIFTextField

--- packages/core/src/components/fields/IIIFTextField/IIIFTextField.stories.js
+++ packages/core/src/components/fields/IIIFTextField/IIIFTextField.stories.js
@@ -1,15 +1,13 @@
 import * as React from 'react';
 import { storiesOf } from '@storybook/react';
-import { withKnobs, text } from '@storybook/addon-knobs';
+import { withKnobs, text, object } from '@storybook/addon-knobs';
 import IIIFTextField from './IIIFTextField.js';
 
 storiesOf('IIIFTextField', module)
   .addDecorator(withKnobs)
   .add('IIIFTextField with string', () => {
-    return (
-      <IIIFTextField
-        classes={{}}
-        label={text('Label', 'Label')}
-      />
-    );
+    return <IIIFTextField
+    classes={object("Classes", {htmlSwitchWrapper: 'htmlSwitchWrapper'} )} 
+    value={text('Value', 'Some Value')}
+    label={text('Label', 'Label')} />;
   });
