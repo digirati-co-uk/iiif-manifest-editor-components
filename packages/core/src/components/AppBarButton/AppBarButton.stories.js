11432b91664c90523029dfbd590c4f3b83ff5248
Kelsie <kelsie.braidwood@digirati.com>
2019-10-10 16:17:00 +0100

    adds an example of a note to the storybook - The Notes are on the top left of the canvas

--- packages/core/src/components/AppBarButton/AppBarButton.stories.js
+++ packages/core/src/components/AppBarButton/AppBarButton.stories.js
@@ -6,16 +6,20 @@ import AppBarButton from './AppBarButton.js';
 
 storiesOf('AppBarButton', module)
   .addDecorator(withKnobs)
-  .add('AppBarButton with Label', () => {
-    return (
-      <AppBarButton
-        icon={':)'}
-        text={text('Text', 'Update me')}
-        onClick={() => {}}
-        variant={'icon-and-label'}
-      />
-    );
-  })
+  .add(
+    'AppBarButton with Label',
+    () => {
+      return (
+        <AppBarButton
+          icon={':)'}
+          text={text('Text', 'Update me')}
+          onClick={() => {}}
+          variant={'icon-and-label'}
+        />
+      );
+    },
+    { notes: 'This is an example of notes added to a story' }
+  )
   .add('AppBarButton Slim', () => {
     return (
       <AppBarButton
