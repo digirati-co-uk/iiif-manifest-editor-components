b8495f57210db56384dddbd8c7f23abf94e60de0
Kelsie <kelsie.braidwood@digirati.com>
2019-10-10 10:46:10 +0100

    Adds IIIFInputField story

---
+++ packages/core/src/components/fields/IIIFInputField/IIIFInputField.stories.js
@@ -0,0 +1,23 @@
+import * as React from 'react';
+import { storiesOf } from '@storybook/react';
+import { withKnobs, text, number} from '@storybook/addon-knobs';
+import IIIFInputField from './IIIFInputField.js';
+
+storiesOf('IIIFInputField', module)
+  .addDecorator(withKnobs)
+  .add('IIIFInputField with string', () => {
+    return (
+      <IIIFInputField
+        value={text('A string value', 'A string value')}
+        onChane={() => {}}
+      />
+    );
+  })
+  .add('IIIFInputField with number', () => {
+    return (
+      <IIIFInputField
+        value={number('A numerical value', 1)}
+        onChange={() => {}}
+      />
+    );
+  });
